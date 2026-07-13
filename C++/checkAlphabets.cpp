a2073e75f08d6ba68bd43c2619bbe7b89b3182c7
Pruthvi <pspawar920@gmail.com>
2026-07-13 11:43:42 +0530

    new code added

---
+++ C++/checkAlphabets.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"Enter the characters : ";
+    cin>>ch;
+    if(ch>='a' && ch<='z' || ch>='A' && ch<='Z'){
+        cout<<ch<<" is a alphabet";
+    }
+    else{
+        cout<<ch<<" is not a alphabet";
+    }
+    return 0;
+}
